6a792ad8919ccfc46a317be20b5eccb2d20a1beb
Anjia Wang <anjiawang@gmail.com>
2018-08-14 12:01:09 -0400

    Adding test case for user defined identifier in REDUCTION clause but failed.
    The error is as follows.
    
    ../parseOmp -rose:C_only --edg:no_warnings -w -rose:verbose 0 --edg:restrict -rose:openmp -rose:openmp:ast_only -I../../../../../../../rose_src/src/frontend/SageIII -c ../../../../../../../rose_src/tests/nonsmoke/functional/CompileTests/OpenMP_tests/cvalidation/for_firstprivate.c
    Error: getType(): default reached: declaration = 0x7f42819cd928 = SgVariableDeclaration
    parseOmp[18654] 0.05937s Rose[FATAL]: assertion failed:
    parseOmp[18654] 0.05942s Rose[FATAL]:   /export/tmp.rose-mgr/jenkins/edg4x/workspace/development-edg-binary-RMC/COMPILER/gcc-5.4.0-default/EDGVERSION/4.9/src/frontend/CxxFrontend/EDG/edgRose/edgRose.C:26170parseOmp[18654] 0.05946s Rose[FATAL]:   SgType* EDG_ROSE_Translation::getType(SgDeclarationStatement*)
    parseOmp[18654] 0.05949s Rose[FATAL]:   required: type != __null

---
+++ tests/nonsmoke/functional/CompileTests/OpenMP_tests/reduction_user_identifier.c
@@ -0,0 +1,41 @@
+/*
+1. A local copy of reduction variable  is made and initialized depending on the op(e.g. 0 for +).
+2. Compiler finds standard reduction expressions containing op and uses them to update the local copy. 
+3. Local copies are reduced into a single value and combined with the original global value.
+
+*/
+#include <stdio.h>
+#ifdef _OPENMP
+#include <omp.h>
+#endif
+
+int mymax(int r,int n) {
+    int m;
+    if (n>r) {
+        m = n;
+    } else {
+        m = r;
+    }
+    return m;
+}
+
+
+int main()
+{
+  int i, m;
+  long sum=0;
+  int total=100;
+
+
+#pragma omp declare reduction \
+    (rwz:int:omp_out=mymax(omp_out,omp_in)) \
+    initializer(omp_priv=INT_MIN)
+    m = INT_MIN;
+#pragma omp parallel reduction(rwz:m)
+    {
+        m = mymax(m, total);
+    }
+
+  return 0;
+}
+
