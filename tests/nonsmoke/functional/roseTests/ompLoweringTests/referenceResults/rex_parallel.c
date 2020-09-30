a1d4229a6ffe66a57aaaa3e10143e28abece84bd
Anjia Wang <anjiawang@gmail.com>
2020-09-30 09:12:56 -0400

    Fix the missing kmpc function call

--- tests/nonsmoke/functional/roseTests/ompLoweringTests/referenceResults/rex_parallel.c
+++ tests/nonsmoke/functional/roseTests/ompLoweringTests/referenceResults/rex_parallel.c
@@ -1,13 +1,14 @@
-#include <assert.h> 
+#include <assert.h>
 #include "rex_kmp.h"
+#include <stdio.h>
 
-void outlined_function_7(int* i, int* j) {
+void outlined_function_7(int* global_id, int* bound_id, int* i, int* j) {
     int __i = *i;
     assert(__i == 100);
     assert(*j == 77);
 }
 
-void outlined_function_12(int* j) {
+void outlined_function_12(int* global_id, int* bound_id, int* j) {
     int i;
     assert(i != 100);
 }
@@ -17,9 +18,9 @@ int main(void)
   int i = 100;
   int j = 77;
 
-  outlined_function_7(&i, &j);
+  __kmpc_fork_call(NULL, 2, outlined_function_7, &i, &j);
 
-  outlined_function_12(&j);
+  __kmpc_fork_call(NULL, 1, outlined_function_12, &j);
 
   return 0;
 }
