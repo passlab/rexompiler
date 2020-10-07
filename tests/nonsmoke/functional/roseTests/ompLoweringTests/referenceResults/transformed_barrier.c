00c49667120cb0ff11f1ad2df50d56b4bdb5486a
Xinyao YI <yxyict@gmail.com>
2020-10-06 23:51:47 -0400

    Update transformed_barrier.c

--- tests/nonsmoke/functional/roseTests/ompLoweringTests/referenceResults/transformed_barrier.c
+++ tests/nonsmoke/functional/roseTests/ompLoweringTests/referenceResults/transformed_barrier.c
@@ -7,7 +7,7 @@ void do_sth()
   printf ("hello.\n");  
 }
 
-void outlined_function()
+void outlined_function(int* global_id, int* bound_id)
 {
     int global_tid = __kmpc_global_thread_num(NULL);
     int tid = omp_get_thread_num();
@@ -21,13 +21,8 @@ void outlined_function()
 }
 
 int main(int argc, char *argv[])
-{
-    //int num_threads = 8;
-    //int current_thread = __kmpc_global_thread_num(NULL);
-    // __kmpc_push_num_threads(NULL, current_thread, num_threads );  
+{ 
     __kmpc_fork_call(NULL, 0, outlined_function);
 
     return 0;
 }
-
-
