6490ee30cc92a1b4e2594f0ba6bb65a6376b22de
Xinyao YI <yxyict@gmail.com>
2020-10-06 23:54:07 -0400

    Update transformed_masterSingle2.c

--- tests/nonsmoke/functional/roseTests/ompLoweringTests/referenceResults/transformed_masterSingle2.c
+++ tests/nonsmoke/functional/roseTests/ompLoweringTests/referenceResults/transformed_masterSingle2.c
@@ -18,10 +18,11 @@ void outlined_function(int* global_id, int* bound_id, int* a, int* b)
 
 int main(int argc, char *argv[])
 {
+    int a,b;
     int num_threads = 8;
     int current_thread = __kmpc_global_thread_num(NULL);
     __kmpc_push_num_threads(NULL, current_thread, num_threads );  
     __kmpc_fork_call(NULL, 2, outlined_function, &a, &b);
-
     return 0;
 }
+
