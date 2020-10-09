568dce6f42dd04da4235b9b297cd8f2b2a52320e
Patrick Flynn <patrickf4664@protonmail.com>
2020-10-09 15:06:24 -0400

    Added conversion for OMP threadprivate

--- src/frontend/SageIII/ompAstConstruction.cpp
+++ src/frontend/SageIII/ompAstConstruction.cpp
@@ -35,6 +35,7 @@ static SgStatement* convertVariantDirective(std::pair<SgPragmaDeclaration*, Open
 static SgOmpBodyStatement* convertBodyDirective(std::pair<SgPragmaDeclaration*, OpenMPDirective*>);
 static SgOmpBodyStatement* convertCombinedBodyDirective(std::pair<SgPragmaDeclaration*, OpenMPDirective*>);
 static SgOmpBodyStatement* convertVariantBodyDirective(std::pair<SgPragmaDeclaration*, OpenMPDirective*>);
+static SgStatement* convertOmpThreadprivateStatement(std::pair<SgPragmaDeclaration*, OpenMPDirective*> current_OpenMPIR_to_SageIII);
 static SgStatement* getOpenMPBlockBody(std::pair<SgPragmaDeclaration*, OpenMPDirective*>);
 static SgOmpVariablesClause* convertClause(SgOmpClauseBodyStatement*, std::pair<SgPragmaDeclaration*, OpenMPDirective*>, OpenMPClause*);
 static void buildVariableList(SgOmpVariablesClause*);
@@ -3320,6 +3321,10 @@ SgStatement* convertDirective(std::pair<SgPragmaDeclaration*, OpenMPDirective*> 
             result = new SgOmpTaskyieldStatement();
             break;
         }
+        case OMPD_threadprivate: {
+            result = convertOmpThreadprivateStatement(current_OpenMPIR_to_SageIII);
+            break;
+        }
         default: {
             printf("Unknown directive is found.\n");
         }
@@ -3727,6 +3732,42 @@ SgOmpBodyStatement* convertBodyDirective(std::pair<SgPragmaDeclaration*, OpenMPD
     };
 
     return result;
+}
+
+// Convert an OpenMPIR Threadprivate Directive to a ROSE node
+// Because we have to do some non-standard things, I'm putting this in a separate function
+SgStatement* convertOmpThreadprivateStatement(std::pair<SgPragmaDeclaration*, OpenMPDirective*> current_OpenMPIR_to_SageIII) {
+    auto statement = new SgOmpThreadprivateStatement();        
+    auto current_ir = static_cast<OpenMPThreadprivateDirective *>(current_OpenMPIR_to_SageIII.second);
+
+    std::vector<const char*>* current_expressions = current_ir->getThreadprivateList();
+    if (current_expressions->size() != 0) {
+        std::vector<const char*>::iterator iter;
+        for (iter = current_expressions->begin(); iter != current_expressions->end(); iter++) {
+            std::string expr_string = std::string() + "varlist " + *iter + "\n";
+            omp_parser_init(current_OpenMPIR_to_SageIII.first, expr_string.c_str());
+            omp_parse();
+        }
+    }
+
+    std::vector<std::pair<std::string, SgNode*> >::iterator iter;
+    for (iter = omp_variable_list.begin(); iter != omp_variable_list.end(); iter++) {
+        if (SgInitializedName* iname = isSgInitializedName((*iter).second)) {
+            SgVarRefExp * var_ref = buildVarRefExp(iname);
+            statement->get_variables().push_back(var_ref);
+            var_ref->set_parent(statement);
+        }
+        else if (SgVarRefExp* vref = isSgVarRefExp((*iter).second)) {
+            statement->get_variables().push_back(vref);
+            vref->set_parent(statement);
+        }
+        else {
+            cerr << "error: unhandled type of variable within a list:" << ((*iter).second)->class_name();
+        }
+    }
+
+    statement->set_definingDeclaration(statement);
+    return statement;
 }
 
 SgOmpAtomicDefaultMemOrderClause* convertAtomicDefaultMemOrderClause(SgOmpClauseBodyStatement* clause_body, std::pair<SgPragmaDeclaration*, OpenMPDirective*> current_OpenMPIR_to_SageIII, OpenMPClause* current_omp_clause) {
@@ -4823,6 +4864,7 @@ bool checkOpenMPIR(OpenMPDirective* directive) {
         case OMPD_section:
         case OMPD_simd:
         case OMPD_parallel_for:
+        case OMPD_threadprivate:
         case OMPD_parallel: {
             break;
         }
