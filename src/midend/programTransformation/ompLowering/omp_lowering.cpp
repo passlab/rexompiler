82b6d7d4fab4a8db6ebfd15d1b06e97ae03c6e48
xinyao <yxyict@gmail.com>
2021-10-24 23:35:07 -0400

    Fixed the error in target parallel for directive

--- src/midend/programTransformation/ompLowering/omp_lowering.cpp
+++ src/midend/programTransformation/ompLowering/omp_lowering.cpp
@@ -3322,6 +3322,8 @@ ASTtools::VarSymSet_t transOmpMapVariables(SgStatement* target_data_or_target_pa
     all_mapped_vars = collectClauseVariables (target_data_stmt, VariantVector(V_SgOmpMapClause));
   else if (target_directive_stmt)
     all_mapped_vars = collectClauseVariables (target_directive_stmt, VariantVector(V_SgOmpMapClause));
+  else if (target_parallel_for_stmt)
+    all_mapped_vars = collectClauseVariables (target_parallel_for_stmt, VariantVector(V_SgOmpMapClause));
 
   // store all variables showing up in any of the device clauses
   SgExpression* device_expression = NULL;
@@ -3329,6 +3331,8 @@ ASTtools::VarSymSet_t transOmpMapVariables(SgStatement* target_data_or_target_pa
     device_expression = getClauseExpression (target_data_stmt, VariantVector(V_SgOmpDeviceClause));
   else if (target_directive_stmt)
     device_expression = getClauseExpression (target_directive_stmt, VariantVector(V_SgOmpDeviceClause));
+  else if (target_parallel_for_stmt)
+    device_expression = getClauseExpression (target_parallel_for_stmt, VariantVector(V_SgOmpDeviceClause));
 
 
   extractMapClauses (map_clauses, array_dimensions, dist_data_policies, &map_alloc_clause, &map_to_clause, &map_from_clause, &map_tofrom_clause);
@@ -3360,7 +3364,20 @@ ASTtools::VarSymSet_t transOmpMapVariables(SgStatement* target_data_or_target_pa
    insertion_scope = isSgBasicBlock(target_directive_stmt->get_scope());
    insertion_anchor_stmt = target_directive_stmt;
   }
+  else if (target_parallel_for_stmt != NULL)
+  {
+   // at this point, the body should already be normalized to be a BB
+   SgBasicBlock * body_block = ensureBasicBlockAsBodyOfOmpBodyStmt(target_parallel_for_stmt);
+   ROSE_ASSERT (body_block!= NULL);
 
+   SgStatement* target_data_child_stmt = NULL;
+   // We cannot assert this since the body of "omp target data" may already be expanded as part of a previous translation
+   //    ROSE_ASSERT( (target_data_stmt_body->get_statements()).size() ==1);
+   target_data_child_stmt = (body_block->get_statements())[0];
+
+   insertion_scope = body_block;
+   insertion_anchor_stmt = target_data_child_stmt;
+  }
   ROSE_ASSERT (insertion_scope != NULL);
   ROSE_ASSERT (insertion_anchor_stmt != NULL);
 
