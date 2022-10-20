f384383af13352fbf50fc3f71019092894ba24a3
Patrick Flynn <pflynn157@gmail.com>
2022-10-20 15:39:06 -0400

    Code cleanup in the SIMD layer

--- src/midend/programTransformation/ompLowering/omp_simd.h
+++ src/midend/programTransformation/ompLowering/omp_simd.h
@@ -32,11 +32,43 @@ enum OpType {
 //
 struct OmpSimdCompiler {
 
-    // Functions
-    bool isStridedLoadStore(SgExpression *pntr_exp);
+public:
+    explicit OmpSimdCompiler(bool isArm);
 
-    // Metadata variables
+    // Functions
+    bool omp_simd_pass1();
+    void omp_simd_pass2();
+    
+    void omp_simd_build_3addr(SgExpression *rval, SgType *type);
+    char omp_simd_get_reduction_mod(SgVarRefExp *var);
+    void omp_simd_build_math(VariantT op_type, SgType *type);
+    void omp_simd_build_scalar_assign(SgExpression *node, SgType *type);
+    void omp_simd_build_ptr_assign(SgExpression *pntr_exp, SgType *type);
+    SgPntrArrRefExp *omp_simd_convert_ptr(SgExpression *pntr_exp);
+    bool isStridedLoadStore(SgExpression *pntr_exp);
+    
+    bool omp_simd_is_load_operand(VariantT val);
+    int omp_simd_get_simdlen(bool safelen);
+    int omp_simd_get_length();
+    
+    // Helper functions
+    SgBasicBlock *getBlock();
+    SgUpirLoopParallelStatement *getTarget();
+    SgForStatement *getForLoop();
+    Rose_STL_Container<SgNode *> *getIR();
+    bool isArm();
+    void setTarget(SgUpirLoopParallelStatement *target);
+    void setForLoop(SgForStatement *for_loop);
+    void addIR(SgNode *ir);
+    
+private:
+    SgBasicBlock *new_block;
+    SgUpirLoopParallelStatement *target = nullptr;
     SgForStatement *for_loop = nullptr;
+    Rose_STL_Container<SgNode *> *ir_block;
+    bool arm = false;
+    
+    std::stack<std::string> nameStack;
 };
 
 //
