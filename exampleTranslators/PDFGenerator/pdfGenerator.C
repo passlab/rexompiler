bc7023442076157ecc77b3e794d049255ef2c7a7
Yonghong Yan <yanyh15@gmail.com>
2022-12-26 16:08:46 -0500

    [REX]: ROSE Pruning: remove three sources files, their headers and tests, and their usage: src/roseSupport/sourceLocationInheritedAttribute.h/C, rangemap.h/C and memoryusage.C

--- exampleTranslators/PDFGenerator/pdfGenerator.C
+++ exampleTranslators/PDFGenerator/pdfGenerator.C
@@ -9,31 +9,29 @@ main ( int argc, char** argv )
   ROSE_INITIALIZE;
 
   vector<string> argvList(argv,argv+argc);
-  // accept -rose:convertFullAST
-  if (CommandlineProcessing::isOption(argvList, "-rose:","convertFullAST",true))
-    CppToPdfTranslator::dumpFullAST = true; 
-    
   if (CommandlineProcessing::isOption(argvList, "","--help",false))
   {
     printf("--------------Tool specific instructions for pdfGenerator----\n");
     printf("To dump AST from input files only :  pdfGenerator your_input_file.c\n");
     printf("To dump full AST, including headers: pdfGenerator -rose:convertFullAST your_input_file.c\n");
     printf("---------------end of tool specific instructions --------------\n");
+    return 1;
   }
-  CppToPdfTranslator c;
-  return c.translate(argvList);
-  //return c.translate(argc,argv);
+  
+  // accept -rose:convertFullAST
+  if (CommandlineProcessing::isOption(argvList, "-rose:","convertFullAST",true))
+    bool dumpFullAST = true; 
+
+  SgProject* project = frontend(argvList);
+  ROSE_ASSERT (project != NULL);
+
+  std::string filename = SageInterface::generateProjectName(project);
+
+  if (project->get_verbose() > 0) {
+    cout << "Generating AST tree (" << numberOfNodes() << " nodes) in file " << filename << ".pdf.\n";
+  }
+
+  AstPDFGeneration astpdfgen;
+  astpdfgen.generateInputFiles(project);
+  return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
